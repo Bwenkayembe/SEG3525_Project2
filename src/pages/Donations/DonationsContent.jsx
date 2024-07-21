3728c984e801561eb3f747d1ed13fc590df15baa
Bwen 3 <bkaye073@uottawa.ca>
2024-07-21 18:49:17 -0400

    donations content p1 done

--- src/pages/Donations/DonationsContent.jsx
+++ src/pages/Donations/DonationsContent.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const organizations = [
-  { name: 'Organization 1', description: 'Description 1', image: '/images/organization1.png' },
+  { name: 'Organization 1', description: '', image: '/images/organization1.png' },
   { name: 'Organization 2', description: 'Description 2', image: '/images/organization2.png' },
   { name: 'Organization 3', description: 'Description 3', image: '/images/organization3.png' },
   { name: 'Organization 4', description: 'Description 4', image: '/images/organization4.png' },
@@ -15,13 +15,17 @@ const organizations = [
 ];
 
 const families = [
-  { name: 'Family 1', description: 'Description 1', image: '/images/family1.png' },
-  { name: 'Family 2', description: 'Description 2', image: '/images/family2.png' },
-  { name: 'Family 3', description: 'Description 3', image: '/images/family3.png' },
-  { name: 'Family 4', description: 'Description 4', image: '/images/family4.png' },
-  { name: 'Family 5', description: 'Description 5', image: '/images/family5.png' },
-  { name: 'Family 6', description: 'Description 6', image: '/images/family6.png' },
-  { name: 'Family 7', description: 'Description 7', image: '/images/family7.png' }
+  { name: 'Congo Bleeding for cobalt: A silent Genocide', description: `The Democratic Republic of Congo is experiencing 
+    severe violence and displacement, with millions affected.`, image: '/images/GFMImage1.png', link: 'https://www.gofundme.com/f/Congo-needs-your-support' },
+  { name: 'Voices for Congo • 30 Days • 30 Hopes', description: `This campaign aims to raise funds to provide clean water, food, and shelter for 
+    displaced Congolese families in East Congo's camps.`, image: '/images/GFMImage2.png', link: 'https://www.gofundme.com/f/voices-for-congo-30-days-30-hopes' },
+  { name: `Dons pour les femmes déplacées à l'est de la RDC`, description: `The Congolicious Foundation is launching the Bibititude program to 
+    empower women displaced by conflict. `, image: '/images/GFMImage3.png', link: 'https://www.gofundme.com/f/dons-pour-les-femmes-deplacees-a-lest-de-la-rdc' },
+  { name: `Help Women and children's in Congo`, description: `Valerie Maganya, a survivor of the Rwandan and Congo
+     genocides, seeks support to provide basic necessities for people in Congo.`, image: '/images/GFMImage5.jpg', link: 'https://www.gofundme.com/f/help-women-and-childrens-of-congo' },
+  { name: `Help My Congolese Family Flee Genocide`, description: `Congolese refugee seeks to raise funds to safely relocate her aunts 
+    and cousins from the violence in Goma, Congo.
+     `, image: '/images/GFMImage4.webp', link: 'https://www.gofundme.com/f/help-my-congolese-family-flee-genocide' },
 ];
 
 const DonationsContent = () => {
@@ -30,7 +34,8 @@ const DonationsContent = () => {
     infinite: false,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+    arrows: true, // Ensure arrows are displayed
   };
 
   return (
@@ -54,37 +59,39 @@ const DonationsContent = () => {
         </div>
       </div>
       <div className="donations-divider">
-      <div id="families-section" className="section">
-        <h2>GO FUND MEs</h2>
-        <Slider {...settings}>
-          {families.map((family, index) => (
-            <div key={index} className="card">
-              <img src={family.image} alt={family.name} className="card-image" />
-              <div className="card-content">
-                <h3 className="card-title">{family.name}</h3>
-                <p className="card-description">{family.description}</p>
+        <div id="families-section" className="section">
+          <h2>GO FUND MEs</h2>
+          <Slider {...settings}>
+            {families.map((family, index) => (
+              <div key={index} className="card">
+                <img src={family.image} alt={family.name} className="card-image" />
+                <div className="card-content">
+                  <h3 className="card-title">{family.name}</h3>
+                  <p className="card-description">{family.description}</p>
+                  <a href={family.link} target="_blank" rel="noopener noreferrer">
+                    <button className="donationscontent-donate-button">Donate</button>
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
-      <div id="organizations-section" className="section">
-        <h2>ORGANIZATIONS</h2>
-        <Slider {...settings}>
-          {organizations.map((organization, index) => (
-            <div key={index} className="card">
-              <img src={organization.image} alt={organization.name} className="card-image" />
-              <div className="card-content">
-                <h3 className="card-title">{organization.name}</h3>
-                <p className="card-description">{organization.description}</p>
+            ))}
+          </Slider>
+        </div>
+        <div id="organizations-section" className="section">
+          <h2>ORGANIZATIONS</h2>
+          <Slider {...settings}>
+            {organizations.map((organization, index) => (
+              <div key={index} className="card">
+                <img src={organization.image} alt={organization.name} className="card-image" />
+                <div className="card-content">
+                  <h3 className="card-title">{organization.name}</h3>
+                  <p className="card-description">{organization.description}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
-
-      </div>
   );
 }
 
